1b17f85cb353c456f69d5b328da4bd7a86c4a390
GBwater <gbwater@outlook.com>
2026-09-19 23:14:50 +0800

    refactor: inline native export macro

--- native/include/eco/collision_api.h
+++ native/include/eco/collision_api.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include "eco/export.h"
 #include <cstdint>
+
+#ifdef AR_WINDOWS
+#define ECO_EXPORT __declspec(dllexport)
+#else
+#define ECO_EXPORT __attribute__((visibility("default")))
+#endif
 
 extern "C" {
 ECO_EXPORT int putCollisionEntity(
